ef0be4fe5ad19a0c439c030e396ca4b655ae850c
Jay Royce <faladejoseph400@gmail.com>
2026-04-04 18:06:26 +0100

    Delete & tweak files

--- src/index.js
+++ src/index.js
@@ -2,29 +2,29 @@
 
 import "./styles.css";
 
-import { loadHome } from "./home.js";
-import { loadMenu } from "./menu.js"
-import { loadAbout } from "./about.js"; 
-import { loadContact } from "./contact.js"; 
+// import { loadHome } from "./home.js";
+// import { loadMenu } from "./menu.js"
+// import { loadAbout } from "./about.js"; 
+// import { loadContact } from "./contact.js"; 
 
-const homeBtn = document.querySelector(".home-btn");
-homeBtn.addEventListener("click",() => {
-    loadHome();
-});
+// const homeBtn = document.querySelector(".home-btn");
+// homeBtn.addEventListener("click",() => {
+//     loadHome();
+// });
 
-const menuBtn = document.querySelector(".menu-btn");
-menuBtn.addEventListener("click",() => {
-    loadMenu();
-});
+// const menuBtn = document.querySelector(".menu-btn");
+// menuBtn.addEventListener("click",() => {
+//     loadMenu();
+// });
 
-const aboutBtn = document.querySelector(".about-btn");
-aboutBtn.addEventListener("click",() => {
-    loadAbout();
-});
+// const aboutBtn = document.querySelector(".about-btn");
+// aboutBtn.addEventListener("click",() => {
+//     loadAbout();
+// });
 
-const contactBtn = document.querySelector(".contact-btn");
-contactBtn.addEventListener("click",() => {
-    loadContact();
-});
+// const contactBtn = document.querySelector(".contact-btn");
+// contactBtn.addEventListener("click",() => {
+//     loadContact();
+// });
 
-loadHome();
+// loadHome();
