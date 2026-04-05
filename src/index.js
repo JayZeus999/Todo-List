9963cf0d7f4cc1dd6a2c4c3e3383334aebb4432f
Jay Royce <faladejoseph400@gmail.com>
2026-04-05 15:04:14 +0100

    Edit sidebar color and spacing

--- src/index.js
+++ src/index.js
@@ -2,15 +2,3 @@
 
 import "./styles.css";
 
-// import { loadHome } from "./home.js";
-// import { loadMenu } from "./menu.js"
-// import { loadAbout } from "./about.js"; 
-// import { loadContact } from "./contact.js"; 
-
-// const homeBtn = document.querySelector(".home-btn");
-// homeBtn.addEventListener("click",() => {
-//     loadHome();
-// });
-
-
-// loadHome();
